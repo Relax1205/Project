fe5ff6133040b8efb98bb2cf1c86b70c4290b2d9
Relax <nikitafon2005@gmail.com>
2025-02-27 22:29:44 +0300

    add readme.md

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import webbrowser
 import numpy as np
 import tensorflow as tf
 from flask import Flask, render_template, request, redirect, url_for, flash
@@ -7,6 +8,7 @@ from flask_bcrypt import Bcrypt
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.models import load_model
+import threading
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "supersecretkey"
@@ -18,7 +20,7 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = "login"
 
-# Модель пользователя
+
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), unique=True, nullable=False)
@@ -28,12 +30,12 @@ class User(db.Model, UserMixin):
 def load_user(user_id):
     return User.query.get(int(user_id))
 
-# Создаем базу данных внутри контекста приложения
+
 with app.app_context():
     db.create_all()
 
-# Загрузка модели классификации
-MODEL_PATH = "garbage_classifier_mobilenetv2.h5"
+
+MODEL_PATH = "garbage_classifier.h5"
 if os.path.exists(MODEL_PATH):
     model = load_model(MODEL_PATH)
     CLASS_NAMES = ["Бумага", "Стекло", "Металл", "Пластик", "Органика", "Другой"]
@@ -41,12 +43,12 @@ else:
     model = None
     CLASS_NAMES = []
 
-# Главная страница
+
 @app.route("/")
 def home():
     return render_template("index.html")
 
-# Регистрация
+
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
@@ -59,7 +61,6 @@ def register():
         return redirect(url_for("login"))
     return render_template("register.html")
 
-# Авторизация
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
@@ -71,7 +72,6 @@ def login():
             flash("Неверные данные!", "danger")
     return render_template("login.html")
 
-# Личный кабинет (защищенная страница)
 @app.route("/profile", methods=["GET", "POST"])
 @login_required
 def profile():
@@ -87,12 +87,10 @@ def profile():
             image_path = url_for("static", filename=filename)
 
             if model:
-                # Обработка изображения
                 img = image.load_img(filepath, target_size=(224, 224))
                 img_array = image.img_to_array(img) / 255.0
                 img_array = np.expand_dims(img_array, axis=0)
 
-                # Предсказание
                 predictions = model.predict(img_array)
                 class_idx = np.argmax(predictions)
                 result = CLASS_NAMES[class_idx]
@@ -101,13 +99,15 @@ def profile():
 
     return render_template("profile.html", result=result, image=image_path)
 
-# Выход
 @app.route("/logout")
 @login_required
 def logout():
     logout_user()
     return redirect(url_for("home"))
 
-# Запуск сервера
+def open_browser():
+    webbrowser.open("http://127.0.0.1:5000/")
+
 if __name__ == "__main__":
+    threading.Timer(1.25, open_browser).start() 
     app.run(debug=True)
